9a89b7ff89078cbb71c8dd08ef46884aebe8bbbc
ChrisP2000 <chrispang0202@gmail.com>
2023-10-09 10:58:12 +0800

    Update r_sql.R

--- r_sql/r_sql.R
+++ r_sql/r_sql.R
@@ -1,4 +1,48 @@
+#install.packages(c("DBI", "RSQLite", "dbplyr"))
+
+library(RSQLite)
+library(DBI)
+library(dplyr)
+
+db <- dbConnect(SQLite(), dbname="airline2.db")
 
 #able to choose the file 
+flights00 <- read.csv(file.choose())
+flights01 <-read.csv(file.choose())
+flights02 <- read.csv(file.choose())
+flights03 <- read.csv(file.choose())
+flights04 <- read.csv(file.choose())
+flights05 <-  read.csv(file.choose())
+
+# is to merge all data tgt with same variable names and data types 
+allFlights <- rbind(flights00, flights01, flights02, flights03, flights04, flights05)
+allFlights
+
 airStore <- read.csv(file.choose())
-airStore
+airlines <- read.csv(file.choose())
+
+#creating database table for allFlights
+dbWriteTable(db, 'allFlightsDb', allFlights)
+
+#writing database command
+dbWriteTable(db, 'airStoredb', airStore)
+
+#Listing out ur database 
+dbListTables(db)
+
+# bGetQuery(conn,"SELECT car_names, hp, cyl FROM cars_data WHERE cyl = 8")
+dbGetQuery(db, 'SELECT * FROM airStoredb LIMIT 3')
+
+# showing which airplane has the lowest associated average departure delay
+dbGetQuery(db, "SELECT flightNum, AVG(DepDelay) As Average_Delay FROM allFlightsDb GROUP BY flightNum" )
+
+
+#showing which cities has the highest number of in flight 
+dbGetQuery(db, "SELECT origin, SUM(taxiin) As highest_no_inbound FROM allFlightsDb GROUP BY origin")
+
+
+#Getting which company has the highest cancelled flights
+dbGetQuery(db, "SELECT Uniquecarrier, SUM(Cancelled) As No_of_cancelled FROM allFlightsDb GROUP BY Uniquecarrier ")
+
+#Getting the highest number of flights. 
+
